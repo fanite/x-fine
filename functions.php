be87b9678b0c2871d674c86b9c8de0fdf83c8f06
Fanite <bellong@vip.qq.com>
2019-04-10 00:40:12 +0800

    sadas

--- functions.php
+++ functions.php
@@ -9,6 +9,110 @@
  * @since 1.0.0
  */
 
+if ( ! function_exists( 'xfine_setup' ) ) :
+  function xfine_setup()
+  {
+    // 加载翻译
+    // load_theme_textdomain( 'xfine', get_template_directory() . '/languages' );
+
+    // feed 支持
+    add_theme_support( 'automatic-feed-links' );
+
+    // 添加自定义Logo支持
+    add_theme_support(
+			'custom-logo',
+			array(
+				'height'      => 240,
+				'width'       => 240,
+				'flex-height' => true,
+			)
+    );
+
+    add_theme_support( 'title-tag' );
+
+    add_theme_support( 'post-thumbnails' );
+		set_post_thumbnail_size( 1200, 9999 );
+
+    add_theme_support(
+			'html5',
+			array(
+				'search-form',
+				'comment-form',
+				'comment-list',
+				'gallery',
+				'caption',
+			)
+    );
+    
+    add_theme_support(
+			'post-formats',
+			array(
+				'aside',
+				'image',
+				'video',
+				'quote',
+				'link',
+				'gallery',
+				'status',
+				'audio',
+				'chat',
+			)
+    );
+    
+    // 支持小widgets
+    add_theme_support( 'customize-selective-refresh-widgets' );
+    
+    // This theme uses wp_nav_menu() in two locations.
+    register_nav_menus(
+			array(
+				'primary' => __( '主菜单', 'xfine' ),
+				'social'  => __( '社交菜单', 'xfine' ),
+			)
+		);
+  }
+endif;
+add_action( 'after_setup_theme', 'xfine_setup' );
+
+// 侧边栏widget
+function xfine_widgets_init() {
+	register_sidebar(
+		array(
+			'name'          => __( 'Sidebar', 'xfine' ),
+			'id'            => 'sidebar-1',
+			'description'   => __( 'Add widgets here to appear in your sidebar.', 'xfine' ),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
+
+	register_sidebar(
+		array(
+			'name'          => __( 'Content Bottom 1', 'xfine' ),
+			'id'            => 'sidebar-2',
+			'description'   => __( 'Appears at the bottom of the content on posts and pages.', 'xfine' ),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
+
+	register_sidebar(
+		array(
+			'name'          => __( 'Content Bottom 2', 'xfine' ),
+			'id'            => 'sidebar-3',
+			'description'   => __( 'Appears at the bottom of the content on posts and pages.', 'xfine' ),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
+}
+add_action( 'widgets_init', 'xfine_widgets_init' );
+
 function xfine_theme_scripts()
 {
   wp_enqueue_style( 'xfine-style', get_stylesheet_uri(), array(), wp_get_theme()->get( 'Version' ) );
--- header.php
+++ header.php
@@ -16,6 +16,58 @@
   <meta charset="<?php bloginfo( 'charset' ); ?>" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
+	<link rel="pingback" href="<?php echo esc_url( get_bloginfo( 'pingback_url' ) ); ?>">
+	<?php endif; ?>
+  <title><?php wp_title( '&mdash;', true, 'right' ); bloginfo( 'name' );?>
+	</title>
   <?php wp_head(); ?>
 </head>
-<body>
+<body <?php body_class(); ?>>
+<?php if ( is_front_page() && is_home() ) : ?>
+  <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+<?php else : ?>
+  <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+  <?php
+endif;
+
+$description = get_bloginfo( 'description', 'display' );
+if ( $description || is_customize_preview() ) :
+  ?>
+  <p class="site-description"><?php echo $description; ?></p>
+<?php endif; ?>
+<?php var_dump(has_nav_menu( 'primary' ));?>
+<?php if ( has_nav_menu( 'primary' ) || has_nav_menu( 'social' ) ) : ?>
+  <button id="menu-toggle" class="menu-toggle"><?php _e( 'Menu', 'xfine' ); ?></button>
+
+  <div id="site-header-menu" class="site-header-menu">
+    <?php if ( has_nav_menu( 'primary' ) ) : ?>
+      <nav id="site-navigation" class="main-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Primary Menu', 'xfine' ); ?>">
+        <?php
+          wp_nav_menu(
+            array(
+              'theme_location' => 'primary',
+              'menu_class' => 'primary-menu',
+            )
+          );
+        ?>
+      </nav><!-- .main-navigation -->
+    <?php endif; ?>
+
+    <?php if ( has_nav_menu( 'social' ) ) : ?>
+      <nav id="social-navigation" class="social-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Social Links Menu', 'xfine' ); ?>">
+        <?php
+          wp_nav_menu(
+            array(
+              'theme_location' => 'social',
+              'menu_class'  => 'social-links-menu',
+              'depth'       => 1,
+              'link_before' => '<span class="screen-reader-text">',
+              'link_after'  => '</span>',
+            )
+          );
+        ?>
+      </nav><!-- .social-navigation -->
+    <?php endif; ?>
+  </div><!-- .site-header-menu -->
+<?php endif; ?>
